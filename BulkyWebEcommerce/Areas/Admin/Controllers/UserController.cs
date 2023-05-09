8662de904dce89de6f3baab82285546f4dd5fc42
Niharika Goswami <niharika.goswami@1rivet.local>
2023-05-09 16:22:06 +0530

    Added RoleManagement Logic

--- BulkyWebEcommerce/Areas/Admin/Controllers/UserController.cs
+++ BulkyWebEcommerce/Areas/Admin/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using BulkyWeb.Models;
 using BulkyWeb.Models.ViewModels;
 using BulkyWeb.Utility;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -16,14 +17,63 @@ namespace BulkyWeb.Areas.Admin.Controllers
     public class UserController : Controller
     {
         private readonly ApplicationDbContext _dbContext;
-        public UserController(ApplicationDbContext dbContext)
+        private readonly UserManager<IdentityUser> _userManager;
+        public UserController(ApplicationDbContext dbContext, UserManager<IdentityUser> userManager)
         {
             _dbContext = dbContext;
+            _userManager = userManager;
         }
         public IActionResult Index()
         { 
             return View();
         }        
+
+        public IActionResult RoleManagement(string userId)
+        {
+            string roleId = _dbContext.UserRoles.FirstOrDefault(u => u.UserId == userId).RoleId;
+
+            RoleManagementViewModel roleManagementViewModel = new RoleManagementViewModel()
+            {
+                ApplicationUser = _dbContext.ApplicationUsers.Include(c => c.Company).FirstOrDefault(u => u.Id == userId),
+                RoleList = _dbContext.Roles.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Name
+                }),
+                CompanyList = _dbContext.Companies.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                })
+            };
+            roleManagementViewModel.ApplicationUser.Role = _dbContext.Roles.FirstOrDefault(u => u.Id == roleId).Name;
+            return View(roleManagementViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult RoleManagement(RoleManagementViewModel roleManagementViewModel)
+        {
+            string roleId = _dbContext.UserRoles.FirstOrDefault(u => u.UserId == roleManagementViewModel.ApplicationUser.Id).RoleId;
+            var oldRole = _dbContext.Roles.FirstOrDefault(u => u.Id == roleId).Name;
+
+            if (!(roleManagementViewModel.ApplicationUser.Role == oldRole))
+            {
+                ApplicationUser applicationUser = _dbContext.ApplicationUsers.FirstOrDefault(u => u.Id == roleManagementViewModel.ApplicationUser.Id);
+                if (roleManagementViewModel.ApplicationUser.Role == SD.Role_Company)
+                {
+                    applicationUser.CompanyId = roleManagementViewModel.ApplicationUser.CompanyId;
+                }
+                if (oldRole == SD.Role_Company)
+                {
+                    applicationUser.CompanyId = null;
+                }
+                _dbContext.SaveChanges();
+
+                _userManager.RemoveFromRoleAsync(applicationUser, oldRole).GetAwaiter().GetResult();
+                _userManager.AddToRoleAsync(applicationUser, roleManagementViewModel.ApplicationUser.Role).GetAwaiter().GetResult();
+            }
+            return RedirectToAction("Index");
+        }
 
         #region API Calls
         [HttpGet]
